d73f2090b1206f407c740b420a6772ac85e6d594
Bohdan Pishchak <93521997+Stereoboi@users.noreply.github.com>
2023-10-25 16:05:20 +0300

    stupid mistake

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   useEffect(() => {
     axios
       .get(
-        "https://raw.githubusercontent.com/Stereoboi/backendless_CMS/main/publick/tabs.json"
+        "https://raw.githubusercontent.com/Stereoboi/backendless_CMS/main/public/tabs.json"
       )
       .then((response) => {
         const sortedData = response.data.sort((a, b) => a.order - b.order);
